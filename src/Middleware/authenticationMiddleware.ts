cfda5b628ee561ae7bf2fc3fce5f5ef193ee5745
SahayakSharma <sahayaksharma6@gmail.com>
2024-11-19 23:30:26 +0530

    fix: middleware

--- src/Middleware/authenticationMiddleware.ts
+++ src/Middleware/authenticationMiddleware.ts
@@ -1,21 +1,16 @@
-import {Request,NextFunction} from "express"
+import { Request, NextFunction } from "express"
 import { verifyToken } from "../Handlers/tokenhandler";
 
-export default function authMiddleware(req:Request,res:any,next:NextFunction){
-    const isMobile = req.headers['user-agent']?.includes('Mobile') || req.headers['X-Client-Type'] === 'mobile';
-    var accessToken=null;
-    if(isMobile){
-        accessToken=req.headers['authorization']
+export default function authMiddleware(req: Request, res: any, next: NextFunction) {
+
+    const accessToken = req.headers['authorization']
+
+    if (!accessToken) {
+        return res.send({ status: 420, message: "Unauthorized Request" })
     }
-    else{
-        accessToken=req.cookies.accessToken
-    }
-    if(!accessToken){
-        return res.send({status:420,message:"Unauthorized Request"})
-    }
-    const validation=verifyToken(accessToken)
-    if(!validation){
-        return res.send({status:420,message:"Unauthorized Request"})
+    const validation = verifyToken(accessToken)
+    if (!validation) {
+        return res.send({ status: 420, message: "Unauthorized Request" })
     }
     next()
 }
